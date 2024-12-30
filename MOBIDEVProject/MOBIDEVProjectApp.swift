c340e6763158998c11d6eed72e91fd228219573e
Boe <boerio99@gmail.com>
2024-12-30 13:25:42 +0100

    Initial Commit

---
+++ MOBIDEVProject/MOBIDEVProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOBIDEVProjectApp.swift
+//  MOBIDEVProject
+//
+//  Created by Daniele Boerio on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MOBIDEVProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
