114ad9da9674bbb6e34c46114ebb822e209ff82e
Boe <boerio99@gmail.com>
2025-01-02 23:30:19 +0100

    first commit

---
+++ MOBIDEVProject/Views/ARViewContainer.swift
@@ -0,0 +1,24 @@
+//
+//  ARViewContainer.swift
+//  IndoorNavigationAStar
+//
+//  Created by Ivan Coppola on 05/10/24.
+//
+
+
+import SwiftUI
+import RealityKit
+import ARKit
+
+struct ARViewContainer: UIViewRepresentable {
+    let arView: ARView // Pass ARView directly
+
+    func makeUIView(context: Context) -> ARView {
+        return arView
+    }
+
+    func updateUIView(_ uiView: ARView, context: Context) {
+        
+    }
+}
+
